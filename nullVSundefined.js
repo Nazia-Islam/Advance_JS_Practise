0b22b37cce68a8a3958c9ac616f90524745a881d
Nazia-Islam <naziasebelly@gmail.com>
2020-08-10 15:08:30 +0600

    null vs undefined cleader concept

---
+++ nullVSundefined.js
@@ -0,0 +1,35 @@
+/*  ------------------           case where undefined arise    --------------- */
+// function add(num1, num2){
+//     console.log(num1 + num2); 
+// }
+
+// let addNum = add(2, 3);
+// console.log(addNum);
+
+
+
+// function add(num1, num2){
+//         console.log(num1 + num2); 
+//     }
+// let addNum = add(2);
+// console.log(addNum);
+
+
+// const person = {name: "dude", id: 10202, address: "hsfgj"};
+// console.log(person.height);
+
+
+
+// let ages = [1,2,3];
+// console.log(ages[11]);
+
+
+
+// let fun = undefined; // not recommended
+// console.log(fun);
+
+
+/*  ---------------    Null  --------------- */
+
+// null means not exist
+// null ami explicitly set korbo
